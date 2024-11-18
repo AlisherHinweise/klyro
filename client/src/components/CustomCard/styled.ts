f7c248d7babd8c2d1b6326ad0825c44e3a53a6ed
3TERRRNITY <3terrrnity@gmail.com>
2024-11-18 20:08:36 +0300

    FE-style-fixes

--- client/src/components/CustomCard/styled.ts
+++ client/src/components/CustomCard/styled.ts
@@ -2,11 +2,11 @@
 import { Card, styled } from '@mui/material'
 
 export const StyledCard = styled(Card)`
-  background-color: #121928;
-  border: 1px solid #141c2e;
-  border-radius: 24px;
-  padding: 24px 48px;
-  -webkit-box-shadow: 5px 5px 8px 3px rgba(34, 60, 80, 0.2);
-  -moz-box-shadow: 5px 5px 8px 3px rgba(34, 60, 80, 0.2);
-  box-shadow: 5px 5px 8px 3px rgba(34, 60, 80, 0.2);
+  background-color: rgba(18, 25, 40, 0.36);
+  border: 1px solid rgba(255, 255, 255, 0.1);
+  border-radius: 16px;
+  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+  backdrop-filter: blur(10.6px);
+  -webkit-backdrop-filter: blur(10.6px);
+  border: 1px solid rgba(63, 137, 255, 0.3);
 `
--- client/src/features/Navbar/styled.ts
+++ client/src/features/Navbar/styled.ts
@@ -3,11 +3,17 @@ import { styled } from '@mui/material'
 
 export const NavbarContainer = styled('nav')`
   display: flex;
-  background-color: #212121;
+  background-color: rgba(33, 33, 33, 0.7);
   color: #ffffff;
   justify-content: space-between;
   align-items: center;
   padding: 16px;
+  border-radius: 16px;
+  backdrop-filter: blur(10.6px);
+  -webkit-backdrop-filter: blur(10.6px);
+  border: 1px solid rgba(63, 137, 255, 0.3);
+  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+  position: relative;
 `
 
 export const LogoContainer = styled('div')`
@@ -22,3 +28,16 @@ export const LogoImage = styled('img')`
   object-fit: cover;
 `
 
+export const NavbarBlur = styled('div')`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 10px;
+  background: linear-gradient(
+    to bottom,
+    rgba(33, 33, 33, 0),
+    rgba(33, 33, 33, 0.3)
+  );
+  border-radius: 0 0 16px 16px;
+`
